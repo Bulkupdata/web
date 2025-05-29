13d7dd8cf5f3b5cfd3c636d976e02127dbdb7d07
Ibeneme <ibenemeikenna96@gmail.com>
2025-05-29 12:28:39 +0100

    updates

--- src/LandingPage/Modal/Modal.tsx
+++ src/LandingPage/Modal/Modal.tsx
@@ -9,31 +9,42 @@ interface ModalProps {
 const Modal = ({ children, onClose }: ModalProps) =>
   ReactDOM.createPortal(
     <div className="modal-overlay" style={{ zIndex: 9999 }}>
-      <button
-        onClick={onClose}
+      <div
         style={{
-          background: "#ff0000",
-          border: "3px solid #fff",
-          fontSize: 18,
-          cursor: "pointer",
-          padding: 8,
-          borderRadius: 122,
-          width: 36,
-          height: 36,
+          width: "100%",
+          maxWidth: 500,
+          alignItems: "center",
+          justifyContent: "flex-end",
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          marginBottom: 8,
-          color: "#fff",
-          alignSelf: "flex-end",
-          marginRight: 12,
         }}
-        aria-label="Close"
       >
-        &times;
-      </button>
-      {children}
+        <button
+          onClick={onClose}
+          style={{
+            background: "#ff0000",
+            border: "3px solid #fff",
+            fontSize: 18,
+            cursor: "pointer",
+            padding: 8,
+            borderRadius: 122,
+            width: 36,
+            height: 36,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: 8,
+            color: "#fff",
+            alignSelf: "flex-end",
+            //   marginRight: 12,
+          }}
+          aria-label="Close"
+        >
+          &times;
+        </button>
+        {children}
+      </div>
     </div>,
     document.body
   );
